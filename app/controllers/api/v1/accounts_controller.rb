039bb3552acba95bbf8fff23011690c32ce70aae
jdaniel231 <jdaniel231@gmail.com>
2025-10-05 23:45:03 -0300

    account

---
+++ app/controllers/api/v1/accounts_controller.rb
@@ -0,0 +1,47 @@
+class Api::V1::AccountsController < ApplicationController
+  before_action :authenticate_request
+  before_action :set_account, only: [:show, :update, :destroy]
+
+  def index
+    @accounts = Account.where(user_id: current_user.id)
+    render json: @accounts
+  end
+
+  def show
+    render json: @account
+  end
+
+  def create
+    @account = Account.new(account_params)
+    @account.user_id = current_user.id
+    if @account.save
+      render json: @account, status: :created
+    else
+      render json: @account.errors, status: :unprocessable_entity
+    end
+  end
+ 
+   def update
+    if @account.update(account_params)
+      render json: @account
+    else
+      render json: @account.errors, status: :unprocessable_entity
+    end
+  end
+    
+     def destroy
+    @account = Account.find(params[:id])
+    @account.destroy
+    render json: @account
+  end
+
+  private
+
+  def set_account
+    @account = Account.find(params[:id])
+  end
+
+  def account_params
+    params.permit(:name)
+  end
+end
